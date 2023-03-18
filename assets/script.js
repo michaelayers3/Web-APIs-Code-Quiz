c230069410dd73a1179cce71838163889ddb66eb
michaelayers3 <michaelgregoryayers@gmail.com>
2023-03-18 12:18:30 -0600

    good god finally got the timer to work. it stops on the time left and I got it to display the final time. now I need to do the local storage stuff. gonna take a quick break though.

--- assets/script.js
+++ assets/script.js
@@ -12,63 +12,47 @@ var secondsStart = 100;
 var scoreEl = document.querySelector("#score");
 var timerInterval;
 
-function setTime() {
+// function setTime() {
 
-    var timerInterval= setInterval(function() {
+//    var timerInterval= setInterval(function() {
+//         secondsStart--;
+//         timeEl.textContent = secondsStart;
+//     }, 1000);
+
+//     if(secondsStart <= 0) {
+//         clearInterval(timerInterval);
+//     }
+// }
+
+function setTime() {
+     timerInterval = setInterval(function() {
         secondsStart--;
         timeEl.textContent = secondsStart;
     }, 1000);
-
-    if(secondsStart <= 0) {
-        clearInterval(timerInterval);
-    }
 }
+
+function stopTime() {
+    clearInterval(timerInterval);
+   scoreEl.textContent = secondsStart;
+
+
+}
+
 
 //Stop time and record time
 
-var scoreEl= document.querySelector("#score");
-
-function saveScore() {
-    var score = clearInterval (function() {
-        clearInterval(timerInterval);
-    scoreEl.textContent = score;
-    localStorage.setItem("score", score);
-    });
-}
-
- 
 
 
-// function setTime() {
-
-//     var timerInterval = setInterval(function() {
-//         secondsStart--;
-//         timeEl.textContent = secondsStart;
-//     if(secondsStart <= 0) {
-        
-//         clearInterval(timerInterval);
-//         gameOverPage.classList.remove("hide");
-//         questionPage.classList.add("hide");
-//         timeContainer.classList.add("hide");
-    
-//     }
-//     }, 1000);
-// }
-
-//button to start game
-var startButton = document.querySelector("#start-btn");
-
-startButton.addEventListener("click", function() {
-    setTime();
-});
 
 //pages that appear and disappear
+var startButton = document.querySelector("#start-btn");
 var startPage = document.querySelector("#start-container");
 var questionPage = document.querySelector("#question-container");
 var gameOverPage = document.querySelector("#game-over-container");
 var timeContainer = document.querySelector("#time-container");
 
 startButton.addEventListener("click", function() {
+    setTime();
     startPage.classList.add("hide");
     timeContainer.classList.remove("hide");
     questionPage.classList.remove("hide");
@@ -81,9 +65,8 @@ var question2Page = document.querySelector("#q2-container");
 var question3Page = document.querySelector("#q3-container");
 var question4Page = document.querySelector("#q4-container");
 var rightanswerButton = document.querySelector("#right-answer-btn");
-// rightanswerButton.addEventListener("click", function() {
-//         secondsStart = secondsStart + 10;
-//     });
+
+
 question1Page.addEventListener("click", function() {
     if (event.target.matches("#right-answer-btn")) {
         secondsStart = secondsStart + 10;
@@ -114,14 +97,15 @@ question3Page.addEventListener("click", function() {
 question4Page.addEventListener("click", function() {
     if (event.target.matches("#right-answer-btn")) {
         secondsStart = secondsStart + 10;
-    }
+       }
     secondsStart = secondsStart - 10;
     question4Page.classList.add("hide");
     gameOverPage.classList.remove("hide");
     questionPage.classList.add("hide");
-    timeContainer.classList.add("hide");
-
+    // timeContainer.classList.add("hide");
+        stopTime();
 });
+
 
 
 
@@ -149,6 +133,10 @@ function renderHighScores() {
 displayHighScores();
 
 submitButton.addEventListener("click", function() {
+    var saveInfo = {
+        initials: initialsInput.value,
+        score: scoreEl.textContent
+    }
     var initials = initialsInput.value
     //add if no initials are entered
      localStorage.setItem("initials", initials)
@@ -163,13 +151,6 @@ function displayHighScores() {
 
 displayHighScores();
 
-
-// var score = secondsLeft;
-// var scoreEl = document.querySelector("#score");
-// scoreEl.textContent = score;
-//     function saveScore() {
-//         localStorage.setItem("score", score);
-//     }
 
 //high score page
 //button to play again
